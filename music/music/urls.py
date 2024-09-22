31d80687d5808f9deb4eae524084aab8c3fca91a
unknown <sebaks992@gmail.com>
2024-09-22 11:27:11 +0200

    Initial commit

---
+++ music/music/urls.py
@@ -0,0 +1,10 @@
+
+# music/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('band.urls')),  # Include band's URLs
+]
+
